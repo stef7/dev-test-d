72a2ee62d75e955f6802e55497744735fe5b570c
stef7 <1480673+stef7@users.noreply.github.com>
2023-06-21 16:38:52 +1000

    removing prisma for now, can come back later if time. also typedRoutes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,10 @@
-import NextAuth, { type AuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET)
   throw new Error("missing google client credentials");
 
 const handler = NextAuth({
-  adapter: PrismaAdapter(prisma) as AuthOptions["adapter"],
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
